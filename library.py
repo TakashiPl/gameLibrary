a1ff54e8a3c2d184b136332e5ae26fd42fd8ae59
Takashi <gadzinam28@gmail.com>
2026-08-02 19:22:12 +0200

    Add batch iteration generator

--- library.py
+++ library.py
@@ -19,12 +19,9 @@ class GameLibrary:
     def filter_by_genre(self, genre: str) -> list[Game]:
         return [game for game in self.games if game.genre.lower() == genre.lower()]
 
-    # def filter_by_min_rating(self, min_rating: int) -> list[Game]:
-    #     return [game for game in self.games if game.rating >= min_rating]
-
     def filter_by_min_rating(self, min_rating: int) -> list[Game]:
-        result = []
-        for game in self.games:
-            if game.rating >= min_rating:
-                result.append(game)
-        return result
+        return [game for game in self.games if game.rating >= min_rating]
+
+    def iter_batches(self, batch_size: int):
+        for i in range(0, len(self.games), batch_size):
+            yield self.games[i : i + batch_size]
