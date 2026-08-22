62a2eec42d52dae0b490bed184a1cb491ec7c9c5
Takashi <gadzinam28@gmail.com>
2026-08-22 18:51:46 +0200

    feat: implement saving model into .joblib file

---
+++ train_and_save.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+import joblib
+
+data = pd.read_csv("data/steam_games_2026.csv")
+data["All_Tags"] = data["All_Tags"].fillna("")
+
+is_recommended = (data["Review_Score_Pct"] >= 85).astype(int)
+
+cat_features = ["All_Tags"]
+num_features = ["Price_USD","Total_Reviews"]
+
+y = is_recommended
+X = data[num_features+cat_features]
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ("num", StandardScaler(), num_features),
+        ("cat", CountVectorizer(max_features=25), "All_Tags")
+    ]
+)
+
+pipeline = Pipeline(
+    steps=[
+        ('transformer',preprocessor),
+        ('regressor',RandomForestRegressor(random_state=42))
+    ]
+)
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,train_size=1)
+
+pipeline.fit(X_train,y_train)
+
+
+joblib.dump(pipeline, "data/steam_model.joblib")
