c3832b8734d77a6198d7c4d2712472c4a4ab673d
Takashi <gadzinam28@gmail.com>
2026-09-01 20:22:40 +0200

    update: add MultiClassClassifier class to pytorch_multiclass python file

--- pytorch_multiclass.py
+++ pytorch_multiclass.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 import torch
+import torch.nn as nn
 
 data = pd.read_csv("data/steam_games_2026.csv")
 
@@ -22,6 +23,29 @@ y = le.fit_transform(genres)
 tensor_x = torch.tensor(X, dtype=torch.float32)
 tensor_y = torch.tensor(y, dtype=torch.long)
 
-print(tensor_x.shape)
-print(tensor_y.shape)
-print(tensor_y.dtype)
+class MultiClassClassifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(2,16)
+        self.fc2 = nn.Linear(16,3)
+        self.relu = nn.ReLU()
+    
+    def forward(self,x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        return x
+
+model = MultiClassClassifier()
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(),lr=0.01)
+epochs = 20
+for e in range(epochs):
+    model.train()
+    optimizer.zero_grad()
+    predictions = model(tensor_x)
+    loss = criterion(predictions, tensor_y)
+    loss.backward()
+    optimizer.step()
+    if e % 5 == 0:
+        print(loss.item(),f"This is the: {e} epoch")
